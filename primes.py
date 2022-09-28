57451854263d66fd0d0e6b75017f60d1faa36cbd
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-09-28 14:56:26 +0100

    add prime functionality and value error exception raise

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError(f'Value Error: The value must be greater that or equal to 1.')
     list = []
+    count=0
+    while count<number_of_primes:
+        for num in range(2,100000000000000):
+            prime = True
+            for i in range(2,num):
+                if num%i==0:
+                    prime = False
+            if prime:
+                list.append(num)
+                count+=1
+                if count>=number_of_primes:
+                    break
     return list
+
+#print(primes(0))
